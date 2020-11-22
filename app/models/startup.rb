c7930bb00e694703a1979ae90c8ef4234a2af952
choim3 <choim3@spu.edu>
2020-11-21 19:51:03 -0800

    done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,84 @@
+require 'pry'
 class Startup
+    attr_reader :founder
+    attr_accessor :name, :domain
+
+    @@all = []
+    @@domain = []
+
+    def initialize(name,founder,domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+        @@domain << domain
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        self.all.find {|start_up| start_up.founder == founder_name}
+    end
+    
+    def self.domains
+        @@domain
+    end
+
+    def sign_contract(venture_capitalist, investment_type, investment_amount)
+        FundingRound.new(self, venture_capitalist, investment_type, investment_amount)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select { |x| x.startup == self}.length
+    end
+
+    # def total_funds
+    #     FundingRound.all.sum { |x| x.investment }
+    # end
+
+    def total_funds
+        total_array = []
+        FundingRound.all.select do |x|
+            if x.startup == self
+                total_array << x.investment
+            end
+        end
+        total_array.sum
+    end
+
+    def investors
+        array = []
+        FundingRound.all.select do |x| 
+            if x.startup == self 
+                array << x.venture_capitalist.name
+            end
+        end
+        array.uniq
+       # self.all.select {|x| x.name == FundingRound.all.startup}
+    end
+
+    def big_investors
+        # i = 0
+        new_array = []
+        VentureCapitalist.tres_commas_club.each do |x|
+            i = 0
+            while i < self.investors.length
+                if x.name == self.investors[i]
+                    new_array << x.name
+                end
+                i += 1
+            end
+        end
+        new_array
+    end
+
+
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,78 @@
+require 'pry'
 class VentureCapitalist
+    
+    attr_accessor :name, :total_worth
+    
+    @@all = []
+    @@worth = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select { |x| x.total_worth >= 1000000000} #.map {|y| y.name}
+    end
+    
+    def offer_contract (startup, investment_type, investment_amount)
+        FundingRound.new(startup, self, investment_type, investment_amount)
+    end
+
+    def funding_rounds
+        FundingRound.all.select { |x| x.venture_capitalist == self }
+    end
+
+    def portfolio
+        array = []
+        FundingRound.all.select do |x| 
+           if x.venture_capitalist == self
+            array << x.startup
+           end
+        end
+        array.uniq
+    end
+
+    def biggest_investment
+        largest_funding = nil
+        max_inv = 0
+        FundingRound.all.each do |x|
+            i = 0
+            while i < self.portfolio.length
+                if x.startup == self.portfolio[i]
+                    if x.investment > max_inv
+                        max_inv = x.investment
+                        largest_funding = x.itself
+                        #binding.pry
+                    end
+                end
+                i += 1
+            end
+        end
+        largest_funding
+    end
+
+    def invested (domain)
+        total_invested = 0
+
+        Startup.all.select do |x| 
+            if x.domain == domain 
+                FundingRound.all.each do |y| 
+                    if y.startup.name == x.name 
+                        total_invested += y.investment
+                        #binding.pry
+                    end
+                end
+            end
+        end
+        total_invested    
+    end
+
+
 
 end
